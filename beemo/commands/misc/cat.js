a0d1d7658a7e2a697807c00753bbfc42ec0cdb2b
Luke James <me@lukej.me>
2017-01-01 14:52:23 +0000

    Fix cat

--- beemo/commands/misc/cat.js
+++ beemo/commands/misc/cat.js
@@ -1,13 +1,9 @@
+const rp = require('request-promise');
+
 module.exports = {
     main: async (bot, message, ...args) => {
-      require('request')("http://random.cat/meow", (error, response, body) => {
-        if(error) {
-          return "Dang, I couldn't get a cat."
-        } else {
-          var image = JSON.parse(body);
-           return 'A cat!!!! '+image.file
-        }
-      })
+        var response = JSON.parse(await rp("http://random.cat/meow"));
+        message.channel.sendFile(response.file);
     },
     help: 'Shows a cute cat!'
 };
