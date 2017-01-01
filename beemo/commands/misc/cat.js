0cfe493b29213f409d831e9c8e19916d0c9a257f
Jacob Bond <jacobpbond@hotmail.co.uk>
2017-01-01 14:28:01 +0000

    Create cat.js

---
+++ beemo/commands/misc/cat.js
@@ -0,0 +1,13 @@
+module.exports = {
+    main: async (bot, message, ...args) => {
+      request("http://random.cat/meow", (error, response, body) => {
+        if(error) {
+          return "Dang, I couldn't get a cat."
+        } else {
+          var image = JSON.parse(body);
+           return 'A cat!!!! '+image.file
+        }
+      })
+    },
+    help: 'Shows a cute cat!'
+};
