a48da45301f51c5f62d412f31c1de311afc68cb2
iovoid <io4@users.noreply.github.com>
2017-01-04 14:30:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const Discord = require('discord.js');
 const chalk = require('chalk');
 const credentials = require('./credentials.json');
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var serverCount = 0;
 
 const manager = new Discord.ShardingManager('beemo/shard.js', {
 	totalShards: credentials.shardCount,
@@ -14,3 +24,53 @@ manager.on('launch', id => manager.log(`Launched Shard ${id.id+1}/${manager.tota
 
 //Start it up
 manager.spawn();
+
+setInterval(_=>{
+manager.fetchClientValues('guilds.size').then(results => {
+  serverCount = results.reduce((prev, val) => prev + val, 0);
+  io.emit("serverCount", serverCount);
+}).catch(console.error);
+},1000);
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+io.on('connection', function (socket) {
+  socket.emit("serverCount", serverCount); // Quickly reply value in cache
+});
