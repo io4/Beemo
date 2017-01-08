f636d8e81c88617dfc783fc22f06f964bcf7e7e7
Luke James <me@lukej.me>
2017-01-08 23:54:37 +0000

    Add +clean

---
+++ beemo/commands/admin/clean.js
@@ -0,0 +1,25 @@
+module.exports = {
+    main: async (bot, message, limit, ...args) => {
+        var limit = bot.resolve.num(limit);
+        if(!limit) {
+            limit = 100;
+        }
+
+        let messages = await message.channel.fetchMessages({limit: limit});
+
+        var user = bot.resolve.user(message);
+        if(user) {
+            messages = messages.array().filter(m => m.author.id === user.id)
+        }
+
+        try {
+            message.channel.bulkDelete(messages);
+        } catch (err) {
+            message.reply("I don't have the Manage Messages permission.");
+        }
+    },
+    help: 'Delete channel messages',
+    guildOnly: true,
+    permissionRequired: 'MANAGE_MESSAGES',
+    args: '[limit] [user]'
+};
