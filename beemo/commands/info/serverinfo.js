335ad1e30175912cba14d6ebc358e402763a000b
wkev <speedo.copp@gmail.com>
2017-01-15 23:26:46 +0000

    (testfn updates)

--- beemo/commands/info/serverinfo.js
+++ beemo/commands/info/serverinfo.js
@@ -1,16 +1,27 @@
 const Discord = require("discord.js");
-
+var vc = (n) => {
+if(n==0) return 'None';
+if(n==1) return 'Low';
+if(n==2) return 'Medium';
+if(n==3) return 'High';
+};
 module.exports = {
 	main: async (bot, message, ...args) => {
+		var members = message.guild.members.filter(m => !m.bot);
+		var boats = message.guild.members.filter(m => m.bot);
+		
 		var embed = new Discord.RichEmbed();
 
 		embed.setTitle(message.guild.name);
 		embed.setColor('#2ecc71');
 		embed.setThumbnail(message.guild.iconURL);
-
+		embed.addField("Owner", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`, true);
 		embed.addField("ID", message.guild.id, true);
-		embed.addField("Members", message.guild.memberCount, true);
-		embed.addField("Roles", message.guild.roles.size, true);
+		embed.addField("Verification Level", vc(message.guild.verificationLevel), true);
+		embed.addField("Members", members.length, true);
+		embed.addField("Bots", boats.length, true);
+		embed.addField(`Roles (${message.guild.roles.length} total)`, message.guild.roles.map(r => r.name).join(', ') || 'None', true);
+		embed.addField(`Emojis (${message.guild.emojis.length} total)`, message.guild.emojis.map(e => e.toString()).join(', ') || 'None', true);
 		embed.addField("Channels", message.guild.channels.size, true);
 
 
