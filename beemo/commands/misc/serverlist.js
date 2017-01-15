929db5b707724c5981cab6c923a6f77f1221a8b7
Cat <kaubariskis123@gmail.com>
2017-01-15 02:54:58 +0200

    fixerino

--- beemo/commands/misc/serverlist.js
+++ beemo/commands/misc/serverlist.js
@@ -5,17 +5,17 @@ module.exports = {
     main: async(bot, message, ...args) => {
         let Embed = new Discord.RichEmbed();
 
-        let res = await bot.shard.broadcastEval(`this.guilds.array()`);
+        let res = await bot.shard.broadcastEval(`this.guilds.map(g => [g.name, g.memberCount])`);
         let list = [];
         let chunks = [];
         res = [].concat(...res);
         res = res.filter((e, i, a) => i === a.indexOf(e));
-        let guilds = res.sort((a, b) => b.memberCount - a.memberCount);
+        let guilds = res.sort((a, b) => b[1] - a[1]);
         guilds.forEach(g => list.push(g))
         while (list.length > 0) chunks.push(list.splice(0, 10));
         let page = Math.min(Math.max(parseInt(message.content), 1), chunks.length) || 1;
 
-        chunks[page - 1].map((g, i) => Embed.addField(`${left(((page - 1) * 10) + (i + 1), 2)}) ${g.name}`, `**${g.memberCount.toLocaleString()} members**`, true));
+        chunks[page - 1].map((g, i) => Embed.addField(`${left(((page - 1) * 10) + (i + 1), 2)}) ${g[0]}`, `**${g[1].toLocaleString()} members**`, true));
         Embed.setAuthor(`Servers I am on (page ${page}/${chunks.length})`, bot.user.avatarURL)
         return Embed;
     },
