5646687b6dfa9c5ea2493bb00deb9476848e2a03
wkev <speedo.copp@gmail.com>
2017-01-16 00:25:46 +0000

    Add emojify command

---
+++ beemo/commands/misc/emojify.js
@@ -0,0 +1,9 @@
+var superagent = require('superagent');
+module.exports = {
+    main: async (bot, message, ...args) => {
+    	 var resp = await superagent.get(`http://emoji.getdango.com/api/emoji?q=${message.content}`);
+        return resp.body.results.map(r => r.text).slice(0, 7).join(' '));
+    },
+    help: 'Emojify your text!', 
+    args: '[text]' 
+};
