b96e369a4d0da76682a55e0206ced703132f3547
wkev <speedo.copp@gmail.com>
2017-01-16 21:39:24 +0000

    Chuck norris command

--- beemo/commands/info/stats.js
+++ beemo/commands/info/stats.js
@@ -5,7 +5,7 @@ module.exports = {
 	    const users = (await bot.shard.broadcastEval('this.guilds.map(g => g.memberCount).reduce((a, b) => a + b)')).reduce((a, b) => a + b);
 	    const channels = (await bot.shard.broadcastEval('this.channels.size')).reduce((a, b) => a + b);
 	    const guilds = (await bot.shard.broadcastEval('this.guilds.size')).reduce((a, b) => a + b);
-
+		const contribs = "";
 	    var embed = new Discord.RichEmbed();
 
 	    embed.setTitle("Beemo Statistics");
@@ -18,9 +18,10 @@ module.exports = {
 	    embed.addField("Guilds", `<:yas:240588984135188481> ${guilds}`, true);
 	    embed.addField("Channels", channels, true);
 	    embed.addField("Users", users, true);
-
-	    embed.addField("Honorable Mentions", "`iovoid#6259, iczero#8740, MoonyTheDwarf#3778, Cat#3204` - Contiburing towards Beemo 3\n\
-`Skiletro#3888, Lopho#4220` - Managing the community and moderating Beemo's HQ.")
+		for(const person in bot.credentials.contributers) {
+			contribs += `${person}: ${bot.credentials.contributers[person]}`
+		};
+	    embed.addField("Honorable Mentions", contribs);
 
 	    return embed;
 	},
