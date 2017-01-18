5260d9ca0a9624b1da343b821c91bb53f76b3d91
Cat <kaubariskis123@gmail.com>
2017-01-19 00:00:04 +0200

    aaaa

--- beemo/commands/admin/softban.js
+++ beemo/commands/admin/softban.js
@@ -1,21 +1,21 @@
 module.exports = {
     main: async (bot, message, ...args) => {
-    	let memberToBan = message.guild.member(bot.resolve.user(message));
+    	let memberToBan = bot.resolve.user(message);
 
-        if(typeof memberToBan == 'undefined') {
-            message.reply("Invalid member.");
+        if(!memberToBan) {
+            message.reply("Invalid user.");
             return;
         }
         try {
-            await memberToBan.ban();
-            await message.guild.unban(memberToBan.user);
+            await memberToBan.ban(7);
+            await message.guild.unban(memberToBan);
         } catch (err) {
             message.reply("Unable to softban, do I have the correct permissions?");
             return;
         }
         message.channel.sendEmbed({description: "<:banne:243432527920889856>"});
     },
-    help: 'Softbans a member',
+    help: 'Softbans a user',
     guildOnly: true,
     permissionRequired: 'BAN_MEMBERS',
     args: '<mention|id|username>'
